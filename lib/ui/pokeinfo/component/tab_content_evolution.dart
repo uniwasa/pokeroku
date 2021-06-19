8fe0f11b15cd0710bd3c60b7d65adafdeec07a8f
nigoro <dolessup@gmail.com>
2021-06-19 17:17:24 +0900

    feat: 進化のビューを改良
    けどまだ改良の余地あり

--- lib/ui/pokeinfo/component/tab_content_evolution.dart
+++ lib/ui/pokeinfo/component/tab_content_evolution.dart
@@ -6,37 +6,41 @@ extension TabContentEvolution on PokeinfoPage {
     List<Widget> evolutionsWidget = [];
     for (final stage in pokemonEx.evolutions) {
       final stageWidget = Expanded(
-          flex: stage.length < 5 ? 3 : 10,
-          child: Container(
-            child: Center(
-              child: Wrap(
-                children: stage.map((stagePokemon) {
-                  final Image pokemonImage = Image.asset(
-                    stagePokemon.imageName,
-                    isAntiAlias: true,
-                    fit: BoxFit.contain,
-                    filterQuality: FilterQuality.none,
-                    width: stage.length < 5 ? double.infinity : null,
-                  );
-                  return InkWell(
-                    onTap: () {
-                      Navigator.pushNamed(context, Routes.pokeinfo,
-                          arguments: stagePokemon);
-                    },
-                    child: Padding(
-                      child: pokemonImage,
-                      padding: EdgeInsets.only(bottom: 10),
+        flex: stage.length < 5 ? 3 : 10,
+        child: Center(
+          child: Wrap(
+            children: stage.map((stagePokemon) {
+              final Image pokemonImage = Image.asset(
+                stagePokemon.imageName,
+                isAntiAlias: true,
+                fit: BoxFit.contain,
+                filterQuality: FilterQuality.none,
+                width: double.infinity,
+              );
+              return ConstrainedBox(
+                constraints: BoxConstraints(maxWidth: 84.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Material(
+                    color: Colors.transparent,
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.pushNamed(context, Routes.pokeinfo,
+                            arguments: stagePokemon);
+                      },
+                      child: Padding(
+                        child: pokemonImage,
+                        padding: EdgeInsets.only(bottom: 10),
+                      ),
                     ),
-                  );
-                }).toList(),
-                alignment: WrapAlignment.start,
-              ),
-            ),
-            decoration: BoxDecoration(
-              color: Theme.of(context).canvasColor,
-              borderRadius: BorderRadius.circular(10),
-            ),
-          ));
+                  ),
+                ),
+              );
+            }).toList(),
+            alignment: WrapAlignment.start,
+          ),
+        ),
+      );
       evolutionsWidget.add(stageWidget);
       evolutionsWidget.add(
         Expanded(
@@ -54,9 +58,18 @@ extension TabContentEvolution on PokeinfoPage {
       SliverToBoxAdapter(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: evolutionsWidget,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Container(
+              color: Theme.of(context).canvasColor,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: evolutionsWidget,
+                ),
+              ),
+            ),
           ),
         ),
       )
