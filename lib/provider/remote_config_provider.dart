8b1a24ade7d098f3df0af221b2ee5697f15e6269
nigoro <dolessup@gmail.com>
2021-12-22 21:59:11 +0900

    ✨ FlavorによってRemote Configの最小フェッチ間隔を変える機能実装

--- lib/provider/remote_config_provider.dart
+++ lib/provider/remote_config_provider.dart
@@ -1,11 +1,15 @@
 import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:pokeroku/provider/flavor_provider.dart';
 
 final remoteConfigProvider = FutureProvider<RemoteConfig>((ref) async {
   final remoteConfig = RemoteConfig.instance;
+  final flavor = ref.watch(flavorProvider);
+  final interval =
+      flavor == Flavor.prod ? const Duration(minutes: 12) : Duration.zero;
   await remoteConfig.setConfigSettings(RemoteConfigSettings(
     fetchTimeout: const Duration(minutes: 1),
-    minimumFetchInterval: const Duration(minutes: 12),
+    minimumFetchInterval: interval,
   ));
   return remoteConfig;
 });
